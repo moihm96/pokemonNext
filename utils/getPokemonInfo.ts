22cd28960e4500b13b8ab5f9c311f22263a4bef3
Moises Heredia Molina <moises.herediamolina@plexus.local>
2022-06-28 12:34:26 +0200

    estabilizacion

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -3,12 +3,17 @@ import { Pokemon } from "../interfaces";
 
 export const getPokemonInfo = async (nameOrId: string) => {
 
+    try {
 
-    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+        const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
 
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites
+        }
+
+    } catch (error) {
+        return null;
     }
 }
